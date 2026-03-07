27c726bc0c4f39368577da110836a139c223606e
srijai16 <kattasrijai16@gmail.com>
2026-03-07 17:18:02 +0530

    Enhance SelectValue with 'truncate' class
    Added 'truncate' class to SelectValue components for better text handling.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -39,9 +39,9 @@ export default function Search() {
 
       {/* Cars */}
       <Select onValueChange={(value) => setCars(value)}>
-        <SelectTrigger className="outline-none border-none w-full shadow-none text-sm md:text-lg truncate">
+        <SelectTrigger className="outline-none border-none w-full shadow-none text-sm md:text-lg">
           
-          <SelectValue placeholder="Cars" />
+          <SelectValue placeholder="Cars" className="truncate" />
         </SelectTrigger>
 
         <SelectContent>
@@ -79,8 +79,8 @@ export default function Search() {
 
       {/* Pricing */}
       <Select onValueChange={(value) => setPrice(value)}>
-        <SelectTrigger className="outline-none border-none w-full shadow-none text-sm md:text-lg truncate">
-          <SelectValue placeholder="Pricing" />
+        <SelectTrigger className="outline-none border-none w-full shadow-none text-sm md:text-lg">
+          <SelectValue placeholder="Pricing" className="truncate" />
         </SelectTrigger>
 
         <SelectContent>
